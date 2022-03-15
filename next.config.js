f1049ae8e4bc2c6407625d984ee468db88820660
Sahibjot Saggu <sahibjot@live.ca>
2022-03-15 02:13:59 -0500

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracing: true,
+  experimental: { outputFileTracing: true },
 };
 
 module.exports = nextConfig;
